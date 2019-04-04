079eaa835fa5aae255ffe8a3eea1398c4eb6897f
JetLu <i@lufei.so>
2019-04-05 00:46:11 +0800

    update

--- ajax.js
+++ ajax.js
@@ -3,14 +3,12 @@ import {noop} from './util'
 export default class {
   method = 'GET'
   responseType = 'text'
-  onreadystatechange = null
+  onreadystatechange = noop
+  abort = noop
+  event = {}
 
   header = {
     Accept: '*/*'
-  }
-
-  constructor() {
-
   }
 
   open(method, url) {
@@ -18,30 +16,68 @@ export default class {
     this.url = url
   }
 
-  send(data) {
-    wx.request({
-      data,
-      url: this.url,
-      header: this.header,
-      method: this.method,
-      responseType: this.responseType,
-      success: info => {
-        this.readyState = 4
-        this.status = statusCode
-        this.response = info.data
-      },
-      fail: info => {
-        this.readyState = 4
-        this.status = info.statusCode
-      }
-    })
-  }
-
   setRequestHeader(key, value) {
     this.header[key] = value
   }
 
-  addEventListener(type, fn) {
+  addEventListener(type, handle) {
+    this.event[type] = this.event[type] || []
+    this.event[type].push(handle)
+  }
 
+  emit(type) {
+    const handles = this.event[type]
+    handles && handles.forEach(handle => {
+      handle.call(this, {target: this})
+    })
+  }
+
+  readFile(path, encoding) {
+    return new Promise((resolve, reject) => {
+      const fs = wx.getFileSystemManager()
+      fs.readFile({
+        encoding,
+        filePath: path,
+        success: info => resolve(info.data),
+        fail: reject
+      })
+    })
+  }
+
+  async send(data) {
+    if (!this.url.match(/^https?/)) {
+      this.readyState = 4
+      this.status = 200
+      this.responseText =
+      this.response = await this.readFile(
+        this.url,
+        this.responseType === 'arraybuffer' ? this.responseType : 'utf-8'
+      )
+      this.emit('readystatechange')
+      this.emit('load')
+    } else {
+      wx.request({
+        data,
+        url: this.url,
+        header: this.header,
+        method: this.method,
+        dataType: 'raw',
+        responseType: this.responseType,
+        success: info => {
+          this.readyState = 4
+          this.status = info.statusCode
+          this.responseText =
+          this.response = info.data
+          this.emit('load')
+          this.emit('readystatechange')
+        },
+        fail: info => {
+          this.readyState = 4
+          this.status = info.statusCode
+          this.emit('error')
+          this.emit('readystatechange')
+        }
+      })
+    }
   }
 }
