61883b2fd3f05486a3e55ddb8ec08c5bc07aa696
无忘 <wuwang@yige-group.com>
2022-06-14 10:45:35 +0800

    update

--- src/element.js
+++ src/element.js
@@ -14,9 +14,10 @@ export class Element {
 
 export const HTMLCanvasElement = canvas.constructor
 
-export const HTMLImageElement = wx.createImage().constructor
+export class HTMLImageElement extends wx.createImage().constructor {
+
+}
 
 export class HTMLVideoElement extends Element {
 
 }
-
